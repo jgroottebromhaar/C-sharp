00ec20edf4efd06d2d1d6735d12a5fde67089679
jgroottebromhaar <jiri_gb_94@hotmail.com>
2019-02-12 08:45:17 +0100

    Opdracht 2 completed

--- SoundSharp/Program.cs
+++ SoundSharp/Program.cs
@@ -7,9 +7,31 @@ namespace SoundSharp
         static void Main(string[] args)
         {
             string nameString;
+            string pass;
             Console.WriteLine("Username");
             nameString = Console.ReadLine();
             Console.WriteLine("Welkom bij SoundSharp " + nameString);
+            Console.WriteLine("Password");
+            pass = Console.ReadLine();
+            Login(pass, nameString);
+        }
+
+        static void Login(string password, string username)
+        {
+            string pass = "SHARPSOUND";
+
+            switch (password == pass)
+            {
+                case true:
+                    Console.WriteLine("Correct password");
+                    break;
+                case false:
+                    Console.WriteLine("Wrong password dickhead");
+                    break;
+                default:
+                    Console.WriteLine("Piss off mate!");
+                    break;
+            }
             Console.ReadLine();
         }
     }
