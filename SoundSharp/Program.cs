2e362d6c299c1c3428a24989a063d243e1d8cfb7
jgroottebromhaar <jiri_gb_94@hotmail.com>
2019-02-13 12:39:44 +0100

    few minor changes

--- SoundSharp/Program.cs
+++ SoundSharp/Program.cs
@@ -10,7 +10,6 @@ namespace SoundSharp
             string pass;
             Console.WriteLine("Username");
             nameString = Console.ReadLine();
-            Console.WriteLine("Welkom bij SoundSharp " + nameString);
             Console.WriteLine("Password");
             pass = Console.ReadLine();
             Login(pass, nameString);
@@ -20,20 +19,27 @@ namespace SoundSharp
         {
             // Password checker
             string pass = "SHARPSOUND";
+            string name = username;
+            int tries = 1;
 
-            switch (password == pass)
-            {
-                case true:
-                    Console.WriteLine("Correct password");
-                    break;
-                case false:
-                    Console.WriteLine("Wrong password dickhead");
-                    break;
-                default:
-                    Console.WriteLine("Piss off mate!");
-                    break;
+            while (tries < 3) {
+                switch (password == pass)
+                {
+                    case true:
+                        Console.WriteLine("Correct password");
+                        Console.WriteLine("Welkom bij SoundSharp " + name);
+                        Console.ReadLine();
+                        break;
+                    case false:
+                        Console.WriteLine("Wrong password");
+                        password = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Piss off mate!");
+                        break;
+                }
+                tries++;
             }
-            Console.ReadLine();
         }
     }
 }
