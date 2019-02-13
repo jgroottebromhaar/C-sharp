c7ca0b0f0a893a274ef9f0ed02ecd13dba331d29
jgroottebromhaar <jiri_gb_94@hotmail.com>
2019-02-13 17:18:37 +0100

    meh

--- SoundSharp/Program.cs
+++ SoundSharp/Program.cs
@@ -27,23 +27,37 @@ namespace SoundSharp
             {
                 password = Console.ReadLine();
                 tries++;
-                switch (password == pass)
+                if (password == pass)
                 {
-                    case true:
-                        Console.WriteLine("Correct password");
-                        Console.WriteLine("Welkom bij SoundSharp " + name);
-                        break;
-                    case false:
-                        if (tries == 2)
-                        {
-                            Console.WriteLine("Wrong password poging 2 van 3");
-                        } else if (tries == 3)
-                        {
-                            Console.WriteLine("Wrong password LET OP: Laatste poging!");
-                        }
-                        break;
+                    Console.WriteLine("Correct password");
+                    Console.WriteLine("Welkom bij SoundSharp " + name);
+                    showMenu();
+                    break;
+                } else if (tries == 2)
+                {
+                    Console.WriteLine("Wrong password poging 2 van 3");
+                } else if (tries == 3)
+                {
+                    Console.WriteLine("Wrong password LET OP: Laatste poging!");
                 }
             } while (tries != 4);
+        }
+
+        private static void showMenu()
+        {
+            string[] lines = {
+                "1. Overzicht MP3 spelers",
+                "2. -",
+                "3. -",
+                "4. -",
+                "5. -",
+                "6. -",
+                "7. -",
+                "8. -",
+                "9. Exit" };
+            Console.WriteLine();
+            foreach (string line in lines)
+                Console.WriteLine(line);
         }
     }
 }
